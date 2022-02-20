9ece5096b825ede85f3c38a72617fdd43ea1667d
sunyunxian <personal.sunyunxian@gmail.com>
2022-02-20 17:37:48 +0800

    add 深度数位大扫除

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
-#!/bash/bin
+#!/usr/bin/env bash
+#
+# Copyright 2022 sunyunxian
+#
+# Hugo run.
+#
+# -u: Treat unset variables as an error when substituting.
+# -x: Print commands and their arguments as they are executed. Just for debug shellscript
+# -e: Exit immediately if a command exits with a non-zero status.
+set -eux
 
 hugo server -D
